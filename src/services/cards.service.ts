068e2d49dee263699292ed8002663dd3212ec4e1
psykedady <psdady@msn.com>
2022-09-11 22:32:57 +0200

    added type range for date

--- src/services/cards.service.ts
+++ src/services/cards.service.ts
@@ -1,5 +1,6 @@
 import { Card } from "src/model/card.model";
 import { PersonalData } from "src/model/personal-data.model";
+import { StringConstant } from "src/model/string.constants.model";
 
 export class CardsService {
 
@@ -8,12 +9,20 @@ export class CardsService {
 			new PersonalData({key:"email principale",value:"psykedady@gmail.com", type:"email"}),
 			new PersonalData({key:"email secondaria",value:"psdady@msn.com", type:"email"}),
 			PersonalData.H_SEP,
-			new PersonalData({key:"Twitter",value:"https://twitter.com/PsykeDady",halfcolumn:true, type:"link"}),
+			new PersonalData({key:"Twitter",value:"twitter.com/PsykeDady",halfcolumn:true, type:"link"}),
 			new PersonalData({key:"github",value:"github.com/PsykeDady",halfcolumn:true, type:"link"}),
 			new PersonalData({key:"Telegram",value:"t.me/PsykeDady", type:"link"}),
 			PersonalData.H_SEP,
 			new PersonalData({key:"Cellulare 1",value:"+39 3663148360", type:"tel"}),
 			new PersonalData({key:"Cellulare 2",value:"+39 3274437030", type:"tel"}),
+		]),
+		new Card("Istruzione", [
+			new PersonalData({key:"Conseguite", value:"", type:"label"}),
+			new PersonalData({key:"2005 - 2011",value:StringConstant.DIPLOMA, type:"range"}),
+			new PersonalData({key:"2011 - 2016",value:StringConstant.LAUREAT, type:"range"}),
+			PersonalData.H_SEP,
+			new PersonalData({key:"Interrotte", value:"", type:"label"}),
+			new PersonalData({key:"2016 - 2022",value:StringConstant.LAUREAM, type:"range"}),
 		])
 	];
 
