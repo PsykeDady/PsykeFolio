cc665c7f0bb755c363aec57e2457394d473f6cad
psykedady <psdady@msn.com>
2022-09-11 20:41:26 +0200

    type on personal info card

--- src/services/cards.service.ts
+++ src/services/cards.service.ts
@@ -5,15 +5,15 @@ export class CardsService {
 
 	private _cards:Card[]=[
 		new Card("Contatti e social", [
-			new PersonalData({key:"email principale",value:"psykedady@gmail.com"}),
-			new PersonalData({key:"email secondaria",value:"psdady@msn.com"}),
+			new PersonalData({key:"email principale",value:"psykedady@gmail.com", type:"email"}),
+			new PersonalData({key:"email secondaria",value:"psdady@msn.com", type:"email"}),
 			PersonalData.H_SEP,
-			new PersonalData({key:"Twitter",value:"https://twitter.com/PsykeDady",halfcolumn:true}),
-			new PersonalData({key:"github",value:"github.com/PsykeDady",halfcolumn:true}),
-			new PersonalData({key:"Telegram",value:"t.me/PsykeDady"}),
+			new PersonalData({key:"Twitter",value:"https://twitter.com/PsykeDady",halfcolumn:true, type:"link"}),
+			new PersonalData({key:"github",value:"github.com/PsykeDady",halfcolumn:true, type:"link"}),
+			new PersonalData({key:"Telegram",value:"t.me/PsykeDady", type:"link"}),
 			PersonalData.H_SEP,
-			new PersonalData({key:"Cellulare 1",value:"+39 3663148360"}),
-			new PersonalData({key:"Cellulare 2",value:"+39 3274437030"}),
+			new PersonalData({key:"Cellulare 1",value:"+39 3663148360", type:"tel"}),
+			new PersonalData({key:"Cellulare 2",value:"+39 3274437030", type:"tel"}),
 		])
 	];
 
