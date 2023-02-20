75e025fe7d7eaf6a16009e2ccb618292c562dded
PsykeDady <psdady@msn.com>
2023-02-20 10:47:51 +0100

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.psyke',
+  appName: 'PsykeFolio',
+  webDir: 'dist/psyke-folio',
+  bundledWebRuntime: false
+};
+
+export default config;
